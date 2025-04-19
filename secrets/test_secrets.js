90d45cb2b984b64c1d9e18c07da32623ce117a95
lingala-sivasaireddy <lingala.sivasaireddy@gmail.com>
2025-04-19 14:00:49 +0530

    Update test_secrets.js

--- secrets/test_secrets.js
+++ secrets/test_secrets.js
@@ -1,6 +1,12 @@
 // This file contains fake but recognizable secrets for 2MS
+// These are fake but realistic secrets for testing 2MS
 
-const aws_key = "AKIAIOSFODNN7EXAMPLE";           // AWS Access Key
-const stripe_key = "sk_test_51H6jX3y8YwD4Y";       // Stripe Test Key
-const github_token = "ghp_abcd1234efgh5678ijkl";   // GitHub Token
-const slack_token = "xoxb-1234-5678-abcd";         // Slack Bot Token
+const AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
+const STRIPE_API_KEY = "sk_live_51H6jX3y8YwD4Y0abcXYZabcXYZ1234567890";
+const GITHUB_TOKEN = "ghp_1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcd";
+const PRIVATE_KEY = `
+-----BEGIN RSA PRIVATE KEY-----
+MIIEpAIBAAKCAQEA7xyzEXAMPLEKEY...
+-----END RSA PRIVATE KEY-----
+`;
+
