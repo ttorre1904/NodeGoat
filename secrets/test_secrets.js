f1afccdb0130b1a937a03df0cc0ecbb00a89f496
lingala-sivasaireddy <lingala.sivasaireddy@gmail.com>
2025-04-19 13:56:37 +0530

    Create test_secrets.js

---
+++ secrets/test_secrets.js
@@ -0,0 +1,6 @@
+// This file contains fake but recognizable secrets for 2MS
+
+const aws_key = "AKIAIOSFODNN7EXAMPLE";           // AWS Access Key
+const stripe_key = "sk_test_51H6jX3y8YwD4Y";       // Stripe Test Key
+const github_token = "ghp_abcd1234efgh5678ijkl";   // GitHub Token
+const slack_token = "xoxb-1234-5678-abcd";         // Slack Bot Token
