89844f3b2b34e697b96050cb22977756a50abb4e
lingala-sivasaireddy <lingala.sivasaireddy@gmail.com>
2025-04-20 05:30:04 +0530

    Create vulns.js
    adding vuln file

---
+++ vulnerable_code/vulns.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/user/:id', function(req, res) {
+  const userId = req.params.id;
+  // Potential SQL Injection vulnerability
+  const query = "SELECT * FROM users WHERE id = '" + userId + "'";
+  db.query(query, function(err, result) {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
