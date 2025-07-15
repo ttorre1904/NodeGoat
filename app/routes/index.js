cb0dd0cc16dbd6be063ba1a868e0a78d98688571
seoonju <tjswn9477@gmail.com>
2025-07-15 18:45:14 +0900

    [Autofic] 3 malicious code detected!!

--- app/routes/index.js
+++ app/routes/index.js
@@ -68,8 +68,13 @@ const index = (app, db) => {
 
     // Handle redirect for learning resources link
     app.get("/learn", isLoggedIn, (req, res) => {
-        // Insecure way to handle redirects by taking redirect url from query string
-        return res.redirect(req.query.url);
+        const allowedUrls = ["https://trustedsite.com", "https://anothertrustedsite.com"];
+        const redirectUrl = req.query.url;
+        if (allowedUrls.includes(redirectUrl)) {
+            return res.redirect(redirectUrl);
+        } else {
+            return res.status(400).send("Invalid redirect URL.");
+        }
     });
 
     // Research Page
